8a4ae4bad05cdb2031e3e354c1f09b791a28d92b
Todor Georgiev <98639854+TodorGeorgiev05@users.noreply.github.com>
2023-03-25 19:33:33 +0200

    NUnit tests v1

---
+++ TestProject1/Services/ScoreServiceTests.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+using Moq;
+using LogRegPage.Models;
+using LogRegPage.Repositories;
+using System.Linq;
+using System.Collections.Generic;
+/*[Test]
+public void TestUpdate()
+{
+// Arrange
+var score = new Score { Id = 1, ScoreGameOne = 10};
+
+// Act
+var result = _scoreRepository.Update(score);
+
+// Assert
+Assert.IsTrue(result);
+_mockGeoQuestContext.Verify(x => x.Update(score), Times.Once);
+_mockGeoQuestContext.Verify(x => x.SaveChanges(), Times.Once);
+}*/
+
+[TestFixture]
+public class ScoreRepositoryTests
+{
+    private Mock<GeoQuestContext> _mockContext;
+    private ScoreRepository _scoreRepo;
+
+    [SetUp]
+    public void Setup()
+    {
+        _mockContext = new Mock<GeoQuestContext>();
+        _scoreRepo = new ScoreRepository(_mockContext.Object);
+    }
+
+    [Test]
+    public void Update_ShouldCallUpdateAndSaveChanges()
+    {
+        // Arrange
+        var score = new Score { Id = 1, ScoreGameOne = 100 };
+
+        // Act
+        _scoreRepo.Update(score);
+
+        // Assert
+        _mockContext.Verify(x => x.Update(score), Times.Once);
+        _mockContext.Verify(x => x.SaveChanges(), Times.Once);
+    }
+
+    [Test]
+    public void FindScoreByUserId_ShouldReturnScore()
+    {
+        // Arrange.
+        var userid = 1;
+        var score = new Score { Id = userid, ScoreGameOne = 100 };
+        var scorelist = new List<Score> { score }.AsQueryable();
+
+        // Act
+        var foo = _scoreRepo.FindScoreByUserId(List< score > );
+
+        // Assert
+        Assert.IsNotNull(foo);
+        Assert.AreEqual(foo.Id, scorelist);
+       /* // arrange
+        var userid = 1;
+        var score = new score { id = userid, scoregameone = 100 };
+        var scorelist = new list<score> { score }.asqueryable();
+
+        _mockcontext.setup(x => x.scores).returns<string>(str => scorelist);
+
+        // act
+        var result = _scorerepo.findscorebyuserid(userid);
+
+        // assert
+        assert.areequal(score, result);
+        _mockcontext.verify(x => x.scores, times.once);*/
+    }
+
+    [Test]
+    public void Delete_ShouldCallRemoveAndSaveChanges()
+    {
+        // Arrange
+        var score = new Score { Id = 1, ScoreGameOne = 100 };
+
+        // Act
+        _scoreRepo.Delete(score);
+
+        // Assert
+        _mockContext.Verify(x => x.Scores.Remove(score), Times.Once);
+        _mockContext.Verify(x => x.SaveChanges(), Times.Once);
+    }
+}
---
+++ TestProject1/Services/UserRepositoryTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LogRegPage.Models;
+using LogRegPage.Repositories;
+using Moq;
+using NUnit.Framework;
+
+[TestFixture]
+public class UserRepositoryTests
+{
+    private Mock<GeoQuestContext> _mockContext;
+    private UserRepository _userRepo;
+
+    [SetUp]
+    public void Setup()
+    {
+        _mockContext = new Mock<GeoQuestContext>();
+        _userRepo = new UserRepository(_mockContext.Object);
+    }
+    
+    [Test]
+    public void GetUser_ShouldReturnUser()
+    {
+        // Arrange
+        var user = new User { Id = 1, UserName = "admin" };
+        var users = new List<User>{ user }.AsQueryable();
+
+       _mockContext.Setup(x => x.Users).Returns(users);
+
+        // Act
+        var result = _userRepo.GetUser(u => u.UserName == "admin");
+
+        // Assert
+        Assert.AreEqual(user, result);
+        _mockContext.Verify(x => x.Users, Times.Once);
+
+    }
+
+    [Test]
+    public void Update_ShouldCallUpdateAndSaveChanges()
+    {
+
+        // Arrange
+        var user = new User { Id = 1, UserName = "admin" };
+
+        // Act
+        _userRepo.Update(user);
+
+        // Assert
+        _mockContext.Verify(x => x.Update(user), Times.Once);
+        _mockContext.Verify(x => x.SaveChanges(), Times.Once);
+    }
+
+    [Test]
+    public void Delete_ShouldCallRemoveAndSaveChanges()
+    {
+        // Arrange
+        var user = new User { Id = 1, UserName = "admin"/*, Grade = "1", Password = "123", ScoreId = 1, UserType = "admin" */};
+
+        // Act
+        _userRepo.Delete(user);
+
+        // Assert
+        _mockContext.Verify(x => x.Users.Remove(user), Times.Once);
+        _mockContext.Verify(x => x.SaveChanges(), Times.Once);
+    }
+}
+
